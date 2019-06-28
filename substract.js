03af1a295520381f2c3ac53890aa1efd52d3d303
rakeshy1116 <yrakeshchowdary1116@gmail.com>
2019-06-28 15:37:47 +0530

    Revert "extra feature in subtract funciton"
    This reverts commit 620c7a1541434c0ed45a612a5fbf71e178c57628.

--- substract.js
+++ substract.js
@@ -1,12 +1,5 @@
-//subtract.js
-
-//return substraction of two numbers
 function subtract(a,b) {
-  if(bMINUSa===true) {
-        return b-a;
-   }
-   else {
-        return a-b;
-}	//TODO
+   return a-b;
+	//TODO
 	//function to subtract two numbers
-};
+}
